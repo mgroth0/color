327115566bf9b6d4b2b6b2dc264f6398a21550df
mgroth0 <mgroth49@gmail.com>
2023-10-04 11:50:02 -0400

    changed src/jvmTest/kotlin/matt/color/test/test.kt

--- src/jvmTest/kotlin/matt/color/test/test.kt
+++ src/jvmTest/kotlin/matt/color/test/test.kt
@@ -3,7 +3,7 @@ package matt.color.test
 
 import matt.color.rgb
 import matt.color.toAwtColor
-import matt.test.JupiterTestAssertions.assertRunsInOneSecond
+import matt.test.assertions.JupiterTestAssertions.assertRunsInOneSecond
 import java.awt.Color
 import kotlin.test.Test
 import kotlin.test.assertEquals
